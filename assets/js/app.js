70080acbb1a9b28fb9d5e8d927c2bdecc51b0ebe
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2024-05-02 09:24:24 +0530

    slider image update

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,30 @@ $(".parent-slider").slick({
         }
     ]
 });
+$(".parent-slider-2").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false
+            }
+        }
+    ]
+});
